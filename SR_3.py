15566e2f8a46a39147a186c90a2ef695d41d743a
ergardergard <113748202+ergardergard@users.noreply.github.com>
2023-03-01 13:34:01 +0300

    Create SR_3.py

---
+++ SR_3.py
@@ -0,0 +1,28 @@
+#sr3_1
+import math
+x1 = float(input('Введите от:'))
+x2 = float(input('и до какого значения рассматриваем функцию:'))
+h = float(input('Введите шаг смещения по оси x:'))
+l = []
+while round(x1, 1) <= x2:
+    l.append(str(round((1 + x1 * x1) / 2 * math.atan(x1) - x1 / 2, 5)) + ',' + ' ' + 'При х = ' + str(round(x1, 2)))
+    x1 += h
+print(l)
+
+def funtext(x1,x2,h)   
+    l = []
+    while round(x1, 1) <= x2:
+        l.append(str(round((1 + x1 * x1) / 2 * math.atan(x1) - x1 / 2, 5)) + ',' + ' ' + 'При х = ' + str(round(x1, 2)))
+        x1 += h
+    print(l)
+funtext(x1 = float(input('Введите от:')),x2 = float(input('и до какого значения рассматриваем функцию:')), h = float(input('Введите шаг смещения по оси x:')))
+
+
+
+
+#sr3_2
+import string
+     file_contents = myfile.read().splitlines()
+     print(file_contents.pop(k))
+textsplit(k=int(input('Введите номер строки из текста>>>')))
+textsplit(k=int(input('Введите номер строки из текста>>>')))
